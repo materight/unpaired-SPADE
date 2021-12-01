7e68823f2079e0f242b6ca8a839ead1c7387c872
materight <matteo.est@gmail.com>
2021-12-01 16:33:08 +0100

    Moved project to new repository

---
+++ models/networks/segmenter.py
@@ -0,0 +1,65 @@
+import os
+import torch
+import torchvision.transforms as T
+from models.networks.base_network import BaseNetwork
+from mit_semseg.models import ModelBuilder, SegmentationModule
+
+
+'''
+Class added by @materight
+Models implementation taken from: https://github.com/CSAILVision/semantic-segmentation-pytorch
+'''
+
+
+def download_weigths_from_url(dst, base_path, model_name, encoder_filename, decoder_filename):
+    model_weights_dirpath = f'{dst}/segmenter-{model_name}/'
+    os.makedirs(model_weights_dirpath, exist_ok=True)
+    encoder_weights_filepath = f'{model_weights_dirpath}/{encoder_filename}.pth'
+    decoder_weights_filepath = f'{model_weights_dirpath}/{decoder_filename}.pth'
+    if not os.path.exists(encoder_weights_filepath):
+        torch.hub.download_url_to_file(url=f'{base_path}/{model_name}/{encoder_filename}.pth', dst=encoder_weights_filepath)
+    if not os.path.exists(decoder_weights_filepath):
+        torch.hub.download_url_to_file(url=f'{base_path}/{model_name}/{decoder_filename}.pth', dst=decoder_weights_filepath)
+    return encoder_weights_filepath, decoder_weights_filepath
+
+
+class BaseADE20KSegmenter(BaseNetwork):
+    def __init__(self, opt, encoder_name, decoder_name, encoder_filname, decoder_filename, fc_dim):
+        super().__init__()
+        ADE20K_BASEPATH = 'http://sceneparsing.csail.mit.edu/model/pytorch/'
+        self.model_name = f'ade20k-{encoder_name}-{decoder_name}'
+
+        encoder_weigths_filepath, decoder_weigths_filepath = '', ''
+        if opt.pretrained_seg:
+            encoder_weigths_filepath, decoder_weigths_filepath = download_weigths_from_url(opt.checkpoints_dir, ADE20K_BASEPATH, self.model_name, encoder_filname, decoder_filename)
+
+        encoder = ModelBuilder.build_encoder(arch=encoder_name, fc_dim=fc_dim, weights=encoder_weigths_filepath)
+        decoder = ModelBuilder.build_decoder(arch=decoder_name, fc_dim=fc_dim, weights=decoder_weigths_filepath, use_softmax=True)
+        crit = torch.nn.NLLLoss(ignore_index=opt.label_nc)
+        self.segmentation_module = SegmentationModule(encoder, decoder, crit)
+
+        self.normalization = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+    def forward(self, data, normalize=False):
+        if normalize:
+            data = self.normalization(data)
+        scores = self.segmentation_module({'img_data': data}, segSize=data.shape[2:])
+        if isinstance(scores, tuple):
+            scores, _ = scores
+        scores = torch.roll(scores, 1, dims=1)
+        return scores
+
+
+class UperNet18Segmenter(BaseADE20KSegmenter):
+    def __init__(self, opt):
+        super().__init__(opt, 'resnet18dilated', 'ppm_deepsup', 'encoder_epoch_20', 'decoder_epoch_20', fc_dim=512)
+
+
+class UperNet50Segmenter(BaseADE20KSegmenter):
+    def __init__(self, opt):
+        super().__init__(opt, 'resnet50', 'upernet', 'encoder_epoch_30', 'decoder_epoch_30', fc_dim=2048)
+
+
+class UperNet101Segmenter(BaseADE20KSegmenter):
+    def __init__(self, opt):
+        super().__init__(opt, 'resnet101', 'upernet', 'encoder_epoch_50', 'decoder_epoch_50', fc_dim=2048)
---
+++ test.py
@@ -0,0 +1,96 @@
+"""
+Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
+Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+"""
+
+import os, subprocess
+import re
+import torch
+import numpy as np
+from collections import OrderedDict
+
+import data
+from options.test_options import TestOptions
+from models.pix2pix_model import Pix2PixModel
+from util.visualizer import Visualizer
+from util import html
+from tqdm import tqdm
+
+from mit_semseg.utils import intersectionAndUnion as compute_intersection_and_union, accuracy as compute_accuracy
+from models.networks.segmenter import UperNet101Segmenter
+
+
+opt = TestOptions().parse()
+
+dataloader = data.create_dataloader(opt)
+
+model = Pix2PixModel(opt)
+model.eval()
+
+segmenter = UperNet101Segmenter(opt).to(opt.gpu_ids[0])
+segmenter.eval()
+
+visualizer = Visualizer(opt)
+
+# create a webpage that summarizes the all results
+results_dir = os.path.join(opt.results_dir, opt.name,
+                       '%s_%s' % (opt.phase, opt.which_epoch))
+webpage = html.HTML(results_dir,
+                    'Experiment = %s, Phase = %s, Epoch = %s' %
+                    (opt.name, opt.phase, opt.which_epoch))
+
+# test
+accuracy_sum, intersection_sum, union_sum, batch_count = 0, 0, 0, 0
+
+print('\nGenerating images...')
+with torch.no_grad():
+    for i, data_i in enumerate(tqdm(dataloader, desc='batch', total=opt.how_many if opt.how_many < np.inf else None)):
+        if i * opt.batchSize >= opt.how_many:
+            break
+        
+        generated, reconstructed_semantics = model(data_i, mode='inference')
+        
+        # evaluate generated images by predicting the semantic segmentation given in input to the generator
+        segmentation_preds = None
+        if opt.evaluate:
+            segmentation_preds = segmenter(generated)
+            segmentation_preds = segmentation_preds.argmax(dim=1).unsqueeze(1)
+            preds, labels = segmentation_preds.detach().cpu(), data_i['label'].detach().cpu()
+            preds[preds == opt.label_nc], labels[labels == opt.label_nc] = -1, -1 # convert back to original label values
+            preds, labels = preds + 1, labels + 1     
+            accuracy, _ = compute_accuracy(preds, labels)
+            intersection, union = compute_intersection_and_union(preds, labels, opt.label_nc)
+            accuracy_sum += accuracy
+            intersection_sum += intersection
+            union_sum += union
+            batch_count += 1
+
+
+        img_path = data_i['path']
+        for b in range(generated.shape[0]):
+            visuals = OrderedDict([('input_label', data_i['label'][b]),
+                                ('reconstructed_semantic', reconstructed_semantics[b] if reconstructed_semantics is not None else None),
+                                ('segmentation_evaluation', segmentation_preds[b] if segmentation_preds is not None else None),
+                                ('synthesized_image', generated[b])])
+            visualizer.save_images(webpage, visuals, img_path[b:b + 1])
+
+    webpage.save()
+
+if opt.evaluate:
+    accuracy = accuracy_sum / batch_count
+    iou = intersection_sum / (union_sum + 1e-10)
+    miou = iou.mean()
+    
+    print('\nComputing FID score...')
+    original_dataroot = f'{opt.dataroot}/images/validation'
+    generated_dataroot = f'{results_dir}/images/synthesized_image'
+    device = 'cpu' if opt.gpu_ids[0] == -1 else f'cuda:{opt.gpu_ids[0]}'
+    process = subprocess.run(['python', '-m', 'pytorch_fid', original_dataroot, generated_dataroot, '--num-workers', str(os.cpu_count()), '--device', str(device), '--batch-size', '1'], stdout=subprocess.PIPE)
+    fid = float(re.search('FID:  ([\d.]*)', process.stdout.decode('utf8').strip()).group(1))
+
+    print('\nResults:')
+    print(f'accuracy: {accuracy*100:.2f}%')
+    print(f'mIoU: {miou*100:.2f}')
+    print(f'FID: {fid:.2f}')
+    with open(f'{results_dir}/metrics.txt', 'w') as file:
+        file.write(f'accuracy: {accuracy*100}\nmIoU: {miou*100}\nFID: {fid}')
