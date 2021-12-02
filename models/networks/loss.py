Fixed loss computation

models/networks/loss.py
@@ -131,7 +131,7 @@ class SegLoss(nn.Module):
         weights = weights[:, :, :, :-1]  # Ignore unknown label
         weights = weights.sum(dim=[0, 1, 2]) + 1
         weights = (torch.tensor(target.shape).prod()) / weights  # Inverse class frequency
-        #values = (1 - prediction)**self.opt.gamma_seg * torch.log(prediction)
-        values = prediction
-        loss = F.nll_loss(values, target, weight=weights, ignore_index=self.opt.label_nc)
+        #prediction = (1 - torch.exp(prediction))**self.opt.gamma_seg * prediction
+        prediction = torch.log(prediction)
+        loss = F.nll_loss(prediction, target, weight=weights, ignore_index=self.opt.label_nc)
         return loss